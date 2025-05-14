e271c3d2c495af4d5dba9be00e951c13f6b2bb45
WesCD-os <--global>
2025-05-14 21:45:36 +0800

    Third Commit

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hello World!</h1>'
+    return ('<h1>NAME : LOUIES ANDRE TABANAO</h1>'
+            '<h1>COURSE : BSIT</h1>'
+            '<h1>SUBJECT : SYSTEM INTEGRATION</h1>')
+
 if __name__ == '__main__':
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
