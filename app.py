c02d82e04cf84e38aefc818eb946458511f0c4cc
WesCD-os <--global>
2025-05-14 21:16:23 +0800

    Second Commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '<h1>Hello World!</h1>'
+if __name__ == '__main__':
+    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
